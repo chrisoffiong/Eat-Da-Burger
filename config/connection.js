93ad23f16716aff01cb4b4ca3987282aeb0891fd
Chris Offiong <chrisoffiong@gmail.com>
2019-03-05 18:33:23 -0500

    revised

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,12 @@
 let mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
+ let   connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "root",
         database: "burgers_db"
     });
-};
+;
 
 connection.connect(function(err) {
     if (err) {

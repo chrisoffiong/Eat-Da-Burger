beba18503c33fb3220a2d8caab7d35db45aa3958
Chris Offiong <chrisoffiong@gmail.com>
2019-03-05 20:05:53 -0500

    revised

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 let mysql = require("mysql");
 
  let connection = mysql.createConnection({
-        host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+        host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         user: "azwqd9cc8p8d6gvs",
         password: "pj3ktbk3nyxeost5",
         database: "gv5y7fy06nyszj9o"
